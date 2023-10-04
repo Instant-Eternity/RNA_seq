4134495abefcf9550b26e485b7016ee539ebf19d
Instant-Eternity <80820204+Instant-Eternity@users.noreply.github.com>
2023-10-04 06:43:01 -0500

    Update 03.STAR.sh

--- 03.STAR.sh
+++ 03.STAR.sh
@@ -5,17 +5,14 @@
 # Created Time: Mon 29 Mar 2021 09:31:56 PM CST
 #########################################################################
 #!/use/bin/bash
-'''
-time STAR --runThreadN 6 \
-		  --runMode genomeGenerate \
-		  --genomeDir /bigdata/wangzhang_guest/chenpeng_project/01_data/07_LDP_clean_data_rnaseq/00_ref/mm10/ \
-		  --genomeFastaFiles /bigdata/wangzhang_guest/chenpeng_project/01_data/07_LDP_clean_data_rnaseq/00_ref/mm.fa \
-		  --sjdbGTFfile /bigdata/wangzhang_guest/chenpeng_project/01_data/07_LDP_clean_data_rnaseq/00_ref/gencode.vM26.annotation.gtf \
-		  --sjdbOverhang 100
-'''
 ref=/bigdata/wangzhang_guest/chenpeng_project/01_data/07_LDP_clean_data_rnaseq/00_ref/mm10/
 data_ABX=/bigdata/wangzhang_guest/chenpeng_project/01_data/07_LDP_clean_data_rnaseq/01_ABX
 data_Ctrl=/bigdata/wangzhang_guest/chenpeng_project/01_data/07_LDP_clean_data_rnaseq/02_Ctrl
 result=/bigdata/wangzhang_guest/chenpeng_project/06_result/06_LDP_RNAseq/04_STAR
 
-time STAR --runThreadN 20 --genomeDir $ref --readFilesCommand gunzip -c --readFilesIn $data_ABX/clp-abx-1_FRAS210074515-1r_1.clean.fq.gz $data_ABX/clp-abx-1_FRAS210074515-1r_2.clean.fq.gz --outSAMtype BAM SortedByCoordinate --outFileNamePrefix $result/ABX-1
+time STAR --runThreadN 20 \
+	--genomeDir $ref \
+ 	--readFilesCommand gunzip \
+  	-c --readFilesIn $data_ABX/clp-abx-1_FRAS210074515-1r_1.clean.fq.gz $data_ABX/clp-abx-1_FRAS210074515-1r_2.clean.fq.gz \
+	--outSAMtype BAM SortedByCoordinate \
+ 	--outFileNamePrefix $result/ABX-1
