457018111a2b2f4f8ec1f480a835dd482273fd23
Instant-Eternity <80820204+Instant-Eternity@users.noreply.github.com>
2023-10-03 22:20:13 -0500

    Update 01.QC.sh

--- 01.QC.sh
+++ 01.QC.sh
@@ -5,6 +5,7 @@
 # Created Time: Thu 25 Mar 2021 09:28:47 AM CST
 #########################################################################
 #!/bin/bash
+
 # Default output path for FastQC and MultiQC reports
 output_path=$PWD
 
@@ -12,7 +13,7 @@ output_path=$PWD
 usage() {
     echo "Usage: $(basename "$0") [-o <output_path>] <input_path1> <input_path2> ... <input_pathN>"
     echo "Options:"
-    echo "  -o <output_path>    Set output path (default: $output_path)"
+    echo "  -o <output_path>    Set output path (default: $PWD)"
     exit 1
 }
 
@@ -38,14 +39,23 @@ if [ "$#" -lt 1 ]; then
     usage
 fi
 
+# Create output directories if they don't exist
+mkdir -p "$output_path/01.FastQC"
+mkdir -p "$output_path/01.MultiQC"
+
 # Loop through provided input paths and run FastQC
 for input_path in "$@"; do
-    echo "Running FastQC for $input_path"
-    time fastqc -o "$output_path" -f fastq "$input_path"/*.fq.gz
+    if [ -d "$input_path" ]; then
+        echo "Running FastQC for files in: $input_path"
+        time fastqc -o "$output_path/01.FastQC" -f fastq "$input_path"/*.fq.gz
+    else
+        echo "Error: Input path '$input_path' not found or is not a directory."
+        exit 1
+    fi
 done
 
 # Run MultiQC to summarize the FastQC results
 echo "Running MultiQC to summarize FastQC reports"
-time multiqc "$output_path" -o "$outputpath"
+time multiqc "$output_path/01.FastQC" -o "$output_path/01.MultiQC"
 
 echo "FastQC and MultiQC analysis completed."
