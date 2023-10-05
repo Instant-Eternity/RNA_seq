4c09962092474a3b3b3f821766698b13a1a35cdb
Instant-Eternity <80820204+Instant-Eternity@users.noreply.github.com>
2023-10-05 02:59:33 -0500

    Update 03.Assemble.sh

--- 03.Assemble.sh
+++ 03.Assemble.sh
@@ -9,10 +9,14 @@
 # Default values for paths and species
 input_dir=$PWD
 output_dir=$PWD
+ref="path/to/your/reference.gtf"  # Specify the path to your reference genome GTF file
 
 # Function to display usage information
 usage() {
     echo "Usage: $(basename "$0") -i <input_directory> -o <output_directory>"
+    echo "Options:"
+    echo "  -i <input_directory>  Path to input directory [default: current directory]"
+    echo "  -o <output_directory> Path to output directory [default: current directory]"
     exit 1
 }
 
@@ -33,35 +37,28 @@ while getopts ":i:o:" opt; do
 done
 
 # Check if required directories exist
-for dir in "$input_dir" "$output_dir"; do
-    if [ ! -d "$dir" ]; then
-        echo "Error: Directory '$dir' not found."
-        usage
-    fi
-done
+if [ ! -d "$input_dir" ] || [ ! -d "$output_dir" ]; then
+    echo "Error: Input or output directory not found."
+    usage
+fi
+
+# Ensure a sample list exists before processing
+if [ ! -f "$output_dir/sample.list" ]; then
+    echo "Error: Sample list not found. Make sure to create the sample list in your output directory."
+    exit 1
+fi
 
 # Create output directories if they don't exist
 mkdir -p "$output_dir/03.StringTie"
 
-# Extract sample names and store them in an array
-samples=()
-for file in "$input_dir"/*.Sorted.bam; do
-    # Extract the part of the filename before the dot
-    sample=$(basename "$file" | cut -d'.' -f1-2)
-    # Check if the sample is not already in the array and add it
-    if [[ ! " ${samples[@]} " =~ " ${sample} " ]]; then
-        samples+=("$sample")
-    fi
-done
-
 # Perform StringTie assembly for each sample
-for sample in "${samples[@]}"; do
+while IFS= read -r "$output_dir/sample.list"; do
     echo "Processing sample: $sample"
     time stringtie "$input_dir/$sample.Sorted.bam" -l "$sample" \
-        -o "$output_dir/03.Assemble/$sample/$sample.gtf" \
+        -o "$output_dir/03.StringTie/$sample/$sample.gtf" \
         -p 28 -G "$ref" \
-        -A "$output_dir/03.Assemble/$sample/$sample.gene_abund.tab" \
+        -A "$output_dir/03.StringTie/$sample/$sample.gene_abund.tab" \
         -B -e
-done
+done < "$output_dir/sample.list"
 
 echo "StringTie assembly completed."
