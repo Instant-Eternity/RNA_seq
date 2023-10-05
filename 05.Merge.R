b4ad01d3e6ef7c9e82d7c65ed963abfa61bd49d2
Instant-Eternity <80820204+Instant-Eternity@users.noreply.github.com>
2023-10-05 04:24:41 -0500

    Update 05.Merge.R

--- 05.Merge.R
+++ 05.Merge.R
@@ -15,42 +15,39 @@ options(stringsAsFactors = FALSE)
 
 # Define command line options
 options <- getopt(
-  command.line = TRUE,
-  options = list(
-    'work-path=' ,
-    'data-path=',
-    'sample-id=',
-    'output-file=',
-    'help'
-  ),
-longnames = c("work-path", "data-path", "sample-id", "output-file", "help"),
-shortnames = c("w", "d", "s", "o", "h"),	
-usage = "Usage: script.R --work-path=<work_path> --data-path=<data_path> --sample-id=<sample_id> --output-file=<output_file>"
+	command.line = TRUE,
+	options = list(
+		'input-path=',
+		'sample-list=',
+		'output-path=',
+		'help'
+	),
+	longnames = c("input-path", "sample-list", "output-path", "help"),
+	shortnames = c("i", "s", "o", "h"),	
+	usage = "Usage: script.R --input-path=<input-path> --sample-list=<sample-list> --output-path=<output-path>"
 )
 
 # Function to print help document
 print_help <- function() {
-  cat("Usage: script.R\n")
-  cat("-w, --work-path=<work_path>   : Path to working directory\n")
-  cat("-d, --data-path=<data_path>   : Path to data files\n")
-  cat("-s, --sample-id=<sample_id>   : Sample ID\n")
-  cat("-o, --output-file=<output_file>: Output file\n")
-  cat("-h, --help                 : Print this help message\n")
-  quit(save = "no")
+    cat("Usage: script.R\n")
+	cat("-i, --input-path=<input-path>   : Path to input directory\n")
+	cat("-s, --sample-list=<sample-list>   : Sample list\n")
+	cat("-o, --output-path=<output-path>: Path to output directory\n")
+	cat("-h, --help                 : Print this help message\n")
+	quit(save = "no")
 }
 
 # Check if help option is provided
 if ("help" %in% names(options)) {
-  print_help()
+	print_help()
 }
 
 # Check if required options are provided
-if (length(options$work.path) == 0 || length(options$data.path) == 0 || length(options$sample.id) == 0 || length(options$output.file) == 0) {
-  stop("Error: All parameters are required. Use -h or --help for usage information.")
+if (length(options$input-path) == 0 || length(options$sample-list) == 0 || length(options$output-path) == 0) {
+	stop("Error: All parameters are required. Use -h or --help for usage information.")
 }
 
 # Access the parameters
-work_path <- options$work.path
-data_path <- options$data.path
-sample_id <- options$sample.id
-output_file <- options$output.file
+input-path <- options$input-path
+sample-list <- options$sample-list
+output-path <- options$output-path
